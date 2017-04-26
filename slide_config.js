bedc45f76714bfd81be907deb24fb915e547bb65
ricky <fatfingererr@gmail.com>
2017-04-26 18:24:41 +0800

    first commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Lispy 簡介', 
+     subtitle: '操作括號不可或缺的工具', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/lispy-cake-logo.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'fatfingererr @ ClojureTW 2017 四月聚會', 
+     company: '', 
+     gplus: 'https://plus.google.com/rcwangtw', 
+     twitter: 'rcwangtw', 
+     www: 'http://rcwangtw.github.io/', 
+     github: 'http://github.com/rcwangtw', 
+   }]
+};
